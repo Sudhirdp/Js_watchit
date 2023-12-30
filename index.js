8a6e1f156268dfba624acd05d007dade39410ac5
Sudhir Patil <106015310+Sudhirdp@users.noreply.github.com>
2023-12-30 22:09:34 +0530

    Update index.js
    prettified

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 #!/usr/bin/env node
-const debounce = require('lodash.debounce')
-const chokidar = require('chokidar')
-const program = require('caporal')
-const fs = require('fs')
-const { spawn } = require('child_process')
-const chalk = require('chalk')
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const chalk = require('chalk');
 
 program
   .version('1.0.0')
   .argument('[filename]', 'Name of a file to execute.')
   .action(async ({ filename }) => {
-    const name = filename || 'index.js'
+    const name = filename || 'index.js';
 
     try {
-      await fs.promises.access(name)
-    } catch (err) {
-      throw new Error(`Could not find the file ${name}`)
+      await fs.promises.access(name);
+    } catch {
+      throw new Error(`Could not find the file ${name}`);
     }
 
-    let proc
+    let proc;
     const start = debounce(() => {
       if (proc) {
-        proc.kill()
+        proc.kill();
       }
       console.log(chalk.blue('>>>>>> Starting process...'))
       proc = spawn('node', [name], { stdio: 'inherit' })
-    }, 150)
+    }, 300);
 
-    chokidar.watch('.').on('add', start).on('change', start).on('unlink', start)
+    chokidar.watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start)
   })
 
-program.parse(process.argv)
+program.parse(process.argv);
